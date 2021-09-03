9a081fabe47485189808ff2ec73099967fa96235
giriprabu <giri.prabu@capestart.com>
2021-09-03 19:35:08 +0530

    Dynamically add values from the JSON file.

--- src/components/CollegeCard.js
+++ src/components/CollegeCard.js
@@ -30,39 +30,48 @@ const Rating = ({ rating, totalrating }) => {
     );
 }
 
+const Offer = () => {
+    return (
+        <div className="offerView fw700"><span className="fw300">Flat</span> Rs<span className="green">2,000</span> off + upto Rs <span className="green">500</span> wallet! to avail... <span className="blue">LOGIN</span></div>
+    );
+}
 
-const CollegeCard = () => {
+
+const CollegeCard = ({ college }) => {
+    console.log(college)
     return (
         <div className='cardContainer'>
             <div className="topContainer">
                 <img className="collegeImage" src={Assets.college_01} alt='college_01' />
-                <div className="promotedBackground">
-                    <span className="white fw700">PROMOTED</span>
-                </div>
+                {college.promoted && 
+                    <div className="promotedBackground">
+                        <span className="white fw700">PROMOTED</span>
+                    </div>
+                } 
                 <Rating rating={3.9} totalrating={5} />
                 <div className="tagsContainer">
-                    <Tags tags={ ["Best College 2018", "2Kms away"] }/>
+                    <Tags tags={ college.tags }/>
                 </div>
                 <div className="rankingContainer white fw500">
-                    #7 in 260 colleges in north campus
+                    {college.ranking}
                 </div>
             </div>
             <div className="bottomContainer">
                 <div className="bottomLeftContainer">
-                    <div className="collegeNameAndRating grey fw700">Hansraj College Delhi University - 1 ★★★★★</div>
-                    <div className="collegeAddress grey fw600">Near Vishwavidyalya Metro Station | <span className="distanceFrom">2 Kms away from bus stand</span></div>
-                    <div className="collegeMatch"><span className="matchNumber green fw800">93% Match: </span><span className="kilometers-text grey fw800">2.5kms </span><span>from GTB Nagar, </span></div>
-                    <div className="offerView fw700"><span className="fw300">Flat</span> Rs<span className="green">2,000</span> off + upto Rs <span className="green">500</span> wallet! to avail... <span className="blue">LOGIN</span></div>
+                    <div className="collegeNameAndRating grey fw700">{college.college_name} ★★★★☆</div>
+                    <div className="collegeAddress grey fw600">{college.nearest_place[0]} | <span className="distanceFrom">{college.famous_nearest_places}</span></div>
+                    <div className="collegeMatch"><span className="matchNumber green fw800">93% Match: </span><span className="kilometers-text grey fw800">2.5kms </span><span>from GTB Nagar, 7 Kms from Rajiv Chowk</span></div>
+                    <Offer />
                 </div>
                 <div className="bottomRightContainer">
                     <div className="originalPriceView">
-                        <span className="orginalPrice grey">₹6,300</span> <span className="offerPercentageView white"><span className="fw800">· </span>20</span>
+                        <span className="orginalPrice grey"> ₹{college.original_fees}</span> <span className="offerPercentageView white"><span className="fw800">· </span>{college.discount}</span>
                     </div>
                     <div className="actualPrice red fw800">
-                        ₹5,768
+                        ₹{college.discounted_fees}
                     </div>
-                    <div className="priceDuration grey fw500">Per Semester (3 Months)</div>
-                    <div className="collegePerks green fw600">Free Cancellation . Free Wifi</div>
+                    <div className="priceDuration grey fw500">{college.amenties}</div>
+                    <div className="collegePerks green fw600">{college.amenties.map(amenity => amenity+"   ")}</div>
                 </div>
             </div>
         </div>
