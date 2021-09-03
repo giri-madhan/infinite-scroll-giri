99e7e5c07546d1af5bba9a6f8ec140723d64bb5d
giriprabu <giri.prabu@capestart.com>
2021-09-03 18:56:38 +0530

    Implemented infinite scroll.

--- src/components/CollegeList.js
+++ src/components/CollegeList.js
@@ -1,17 +1,34 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 // custom components
 import CollegeCard from '../components/CollegeCard';
+// Custom Hooks
+import useOnScreen from '../customHooks/useOnScreen';
 // styles 
 import '../styles/collegesList.css'
 import { colleges } from "../data/colleges.json";
 
 const CollegeList = () => {
-    const collegeListRef = useRef();
+    const messagesEndRef = useRef(null);
+    const isVisible = useOnScreen(messagesEndRef);
+    const [showColleges, setShowCollges] = useState([]);
+    const [loadCount, setLoadCount] = useState(1);
+
+    const loadMore = () => {
+        setShowCollges(colleges.slice(0, loadCount * 10))
+        setLoadCount(loadCount + 1);
+    }
+
+    useEffect(() => {
+        loadMore();
+    }, [isVisible])
+    
     return (
-        <div className='collegeFeedRow' ref={collegeListRef}>
-            {colleges.map((college) => 
-                <CollegeCard college={college}/> 
+        <div className='collegeFeedRow'>
+            {showColleges.map((college) => 
+                <CollegeCard college={college} /> 
             )}
+            {/* to check if bottom reached */}
+            <div ref={messagesEndRef} />
         </div>
     );
 }
